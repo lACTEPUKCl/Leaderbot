bcff0a3baeea07cd3f3ba2571d550d81f1fb3cfe
ACTEPUKC <makakakakala@gmail.com>
2023-02-08 22:25:35 +0200

    fix

--- vip-creater.js
+++ vip-creater.js
@@ -4,15 +4,17 @@ import { config } from "dotenv";
 config();
 const adminsCfgPath = process.env.ADMINS_URL;
 const vipCreater = async (steamID, nickname, time, summ, discordId) => {
+  const options = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  };
   console.log("vipcreated");
   const summPerDay = summ / 9.863;
   const currentTime = new Date().getTime();
   const updatedTIme = new Date(currentTime + summPerDay * 24 * 60 * 60 * 1000);
-  const endTime = updatedTIme.toLocaleDateString();
-  const reformData = endTime.split("/");
-  const newdate = reformData[1] + "/" + reformData[0] + "." + reformData[2];
-  console.log(newdate);
-  console.log(updatedTIme.toLocaleDateString());
+  const getTime = updatedTIme.toLocaleDateString("en-GB", options);
+  const endTime = getTime.replace(/\//g, ".");
   fs.readFile(`${adminsCfgPath}Admins.cfg`, "utf-8", (err, data) => {
     if (err) {
       console.error(err);
@@ -40,13 +42,13 @@ const vipCreater = async (steamID, nickname, time, summ, discordId) => {
             "\r\n Создан бэкап файла AdminsBackup.cfg\r\n"
           );
 
-          // exec("../syncconfig.sh", (err, stdout, stderr) => {
-          //   if (err) {
-          //     console.error(err);
-          //     return;
-          //   }
-          //   console.log(stdout);
-          // });
+          exec("../syncconfig.sh", (err, stdout, stderr) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log(stdout);
+          });
         });
       });
     }
