a023c0370c8896656e4c3ca514932ba53bf7d92b
ACTEPUKC <makakakakala@gmail.com>
2023-02-08 20:26:58 +0200

    leaderbot

---
+++ index.js
@@ -0,0 +1,184 @@
+import { Client, GatewayIntentBits, EmbedBuilder } from "discord.js";
+import { config } from "dotenv";
+config();
+import creater from "./vip-creater.js";
+import cleaner from "./vip-cleaner.js";
+
+import {
+  setTimeout as setTimeoutPromise,
+  setInterval,
+} from "node:timers/promises";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+});
+let stats = [];
+let injectKd = [];
+let statsSort = [];
+let players = [];
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  const channel = client.channels.cache.get("1069615679281561600");
+  async function sortUsers(sort) {
+    try {
+      let response = await fetch(process.env.FIREBASE_JSON);
+      const users = await response.json();
+      for (const key in users) {
+        stats.push(users[key]);
+      }
+      stats = Object.values(stats);
+      for (const key in stats) {
+        stats[key].kd = stats[key].kills / stats[key].death;
+        injectKd.push(stats[key]);
+        if (injectKd[key].kills > 500) {
+          statsSort.push(stats[key]);
+        }
+      }
+
+      const sortBy = statsSort.sort((a, b) => (a[sort] < b[sort] ? 1 : -1));
+      for (const key in sortBy) {
+        const a = sortBy[key];
+        players.push(
+          `(${key}) ` +
+            a.name +
+            ": У: " +
+            a.kills +
+            " С: " +
+            a.death +
+            " П: " +
+            a.revives +
+            " ТK: " +
+            a.teamkills +
+            " K/D: " +
+            a.kd.toFixed(2)
+        );
+        if (key === "20") return;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Редактирование Embed
+  async function editEmbed(sort, messageId, authorName, seconds) {
+    setTimeout(async () => {
+      await sortUsers(`${sort}`);
+      await channel.messages
+        .fetch(`${messageId}`)
+        .then((message) => {
+          const playersTable = Array(20)
+            .fill(0)
+            .map((e, i) => players[i + 1])
+            .join("\r\n");
+          let exampleEmbed = new EmbedBuilder()
+            .setAuthor({
+              name: `${authorName}`,
+              iconURL:
+                "https://cdn.discordapp.com/icons/735515208348598292/21416c8e956be0ffed0b7fc49afc5624.webp",
+            })
+            .setDescription(playersTable)
+            .setColor(0x0099ff)
+            .setTimestamp()
+            .setFooter({
+              text: "Статистика обновлялась",
+              iconURL:
+                "https://cdn.discordapp.com/icons/735515208348598292/21416c8e956be0ffed0b7fc49afc5624.webp",
+            });
+          message.edit({ embeds: [exampleEmbed] });
+          stats = [];
+          injectKd = [];
+          statsSort = [];
+          players = [];
+        })
+        .catch(console.error);
+    }, seconds);
+  }
+
+  async function getStats() {
+    const kills = editEmbed(
+      "kills",
+      "1069615769610108938",
+      "Топ 20 игроков по убийствам",
+      1000
+    );
+    const death = editEmbed(
+      "death",
+      "1069615861582811178",
+      "Топ 20 игроков по смертям",
+      2000
+    );
+    const revives = editEmbed(
+      "revives",
+      "1069615953438048276",
+      "Топ 20 медиков",
+      3000
+    );
+    const teamkills = editEmbed(
+      "teamkills",
+      "1069616004457578627",
+      "Топ 20 тимкилеров",
+      4000
+    );
+    const kd = editEmbed(
+      "kd",
+      "1069616217884741693",
+      "Топ 20 игроков по соотношению убийств к смертям",
+      5000
+    );
+    Promise.all([kills, death, revives, kd]);
+  }
+  // Редактирование Embed
+
+  async function startEmbedEdit() {
+    const interval = 3600000;
+    for await (const startTime of setInterval(interval, getStats())) {
+      console.log("Статистика обновлена");
+      getStats();
+    }
+  }
+  startEmbedEdit();
+  let guild = client.guilds.cache.get("735515208348598292");
+  cleaner.vipCleaner((ids) =>
+    ids.forEach((element) => {
+      let user = guild.members.cache.get(element);
+      guild.members.fetch({ user, cache: true }).then().catch(console.error);
+      user?.roles.remove("1072902141666136125");
+    })
+  );
+  //reaction
+  client.on("messageCreate", (message) => {
+    const filter = (reaction, user) => {
+      return (
+        ["👍"].includes(reaction.emoji.name) && user.id === "132225869698564096"
+      );
+    };
+
+    message.awaitReactions({ filter, max: 1 }).then((collected) => {
+      const reaction = collected.first();
+
+      if (reaction?.emoji?.name === "👍") {
+        const objMessage = message.content.split("\n");
+        const nickname = objMessage[0].trim();
+        const steamID = objMessage[1].trim();
+        const time = objMessage[2].trim();
+        const summ = objMessage[3].trim();
+        const discordId = message.author.id;
+        creater.vipCreater(steamID, nickname, time, summ, discordId);
+        let role = message.guild.roles.cache.get("1072902141666136125");
+        let user = message.guild.members.cache.get(message.author.id);
+        user.roles.add(role);
+        //message.delete();
+      }
+    });
+  });
+  //reaction
+});
+client.login(process.env.CLIENT_TOKEN);
---
+++ vip-cleaner.js
@@ -0,0 +1,72 @@
+import fs from "fs";
+
+const regexp =
+  /^Admin=(?<steamID>[0-9]*):Reserved [//]* DiscordID (?<discordId>[0-9]*) do (?<date>[0-9]{2}\.[0-9]{2}\.[0-9]{4})/gm;
+
+const getUserRegExp = (steamID) => {
+  return new RegExp(
+    `Admin=(?<steamID>${steamID}):Reserved [//]* DiscordID (?<discordId>[0-9]*) do (?<date>[0-9]{2}\\.[0-9]{2}\\.[0-9]{4})`
+  );
+};
+const adminsCfgPath = "./";
+const vipCleaner = (callback) => {
+  setInterval(() => {
+    const isPaidDate = (date) => {
+      const lastVipDate = new Date(Date.parse(`${date} GMT`));
+      const currentDate = new Date();
+      return lastVipDate > currentDate;
+    };
+
+    fs.readFile(`${adminsCfgPath}Admins.cfg`, "utf-8", (err, data) => {
+      if (err) return;
+
+      let newData = "";
+      let steamIDForRemove = [];
+      let usersRemove = [];
+      let usersRemoveId = [];
+      const vips = data.matchAll(regexp);
+      for (let result of vips) {
+        const { steamID, date } = result.groups;
+        const reverseDate = date.split(".").reverse().join(".");
+
+        if (!isPaidDate(reverseDate)) {
+          steamIDForRemove.push(steamID);
+        }
+      }
+
+      if (steamIDForRemove.length) {
+        let users = data.split("\r\n");
+
+        steamIDForRemove.forEach((e) => {
+          const userString = data.match(getUserRegExp(e));
+          usersRemove.push(userString[0]);
+          users = users.filter((e) => userString[0] !== e);
+          usersRemoveId.push(userString.groups.discordId);
+        });
+        callback(usersRemoveId);
+        newData = users.join("\r\n");
+      }
+      if (newData.length) {
+        fs.writeFile(`${adminsCfgPath}Admins.cfg`, newData, (err) => {
+          if (err) return;
+          console.log("\x1b[33m", "\r\n Удаленные юзеры:\r\n");
+          usersRemove.forEach((e) => {
+            console.log("\x1b[36m", e);
+          });
+
+          fs.writeFile(`${adminsCfgPath}AdminsBackup.cfg`, data, (err) => {
+            if (err) return;
+
+            console.log(
+              "\x1b[33m",
+              "\r\n Создан бэкап файла AdminsBackup.cfg\r\n"
+            );
+          });
+        });
+      }
+    });
+  }, 8640000);
+};
+export default {
+  vipCleaner,
+};
