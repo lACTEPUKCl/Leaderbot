f3aa40f32a2087fd9d2681d0090dce7630bbf145
ACTEPUKC <makakakakala@gmail.com>
2023-03-22 00:52:17 +0200

    revorkebot

---
+++ editEmbed.js
@@ -0,0 +1,40 @@
+import { EmbedBuilder } from "discord.js";
+import sortUsers from "./sortUsers.js";
+
+async function editEmbed({
+  channel,
+  db,
+  sort,
+  messageId,
+  authorName,
+  seconds,
+}) {
+  setTimeout(async () => {
+    const players = await sortUsers(db, sort);
+    await channel.messages
+      .fetch(`${messageId}`)
+      .then((message) => {
+        const playersTable = Array(20)
+          .fill(0)
+          .map((e, i) => players[i + 1])
+          .join("\r\n");
+        let exampleEmbed = new EmbedBuilder()
+          .setAuthor({
+            name: `${authorName}`,
+            iconURL:
+              "https://cdn.discordapp.com/icons/735515208348598292/21416c8e956be0ffed0b7fc49afc5624.webp",
+          })
+          .setDescription(playersTable)
+          .setColor(0x0099ff)
+          .setTimestamp()
+          .setFooter({
+            text: "Статистика обновлялась",
+            iconURL:
+              "https://cdn.discordapp.com/icons/735515208348598292/21416c8e956be0ffed0b7fc49afc5624.webp",
+          });
+        message.edit({ embeds: [exampleEmbed] });
+      })
+      .catch(console.error);
+  }, seconds);
+}
+export default editEmbed;
---
+++ fetchDonate.js
@@ -0,0 +1,26 @@
+import fs from "fs";
+import creater from "./vip-creater.js";
+
+function addTransaction(tempSteamId, jsonData) {
+  const [name, discorId, steamId, message] = tempSteamId;
+  const { id, sum } = jsonData;
+  fs.readFile(`./transactionId.json`, (err, data) => {
+    if (err) return;
+    let transaction = JSON.parse(data);
+    const test = transaction.transactions.find((e) => e.id === id.toString());
+    if (!test) {
+      transaction.transactions.push({
+        id: `${id}`,
+        name,
+        steamID: steamId,
+      });
+      creater.vipCreater(steamId, name, sum, discorId);
+      message.react("👍");
+    }
+    let newData = JSON.stringify(transaction);
+    fs.writeFile(`./transactionId.json`, newData, (err) => {
+      if (err) return;
+    });
+  });
+}
+export default addTransaction;
