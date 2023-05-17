8bbeff9794c8366246d687139ac0dfc02acf36e4
ACTEPUKC <makakakakala@gmail.com>
2023-05-17 22:57:18 +0300

    change embed

--- getStatsOnDiscord.js
+++ getStatsOnDiscord.js
@@ -1,10 +1,15 @@
 import { MongoClient } from "mongodb";
 import { EmbedBuilder } from "discord.js";
 
-async function getStatsOnDiscord(db, steamId, message) {
+async function getStatsOnDiscord(db, steamId, message, steamApi) {
   const clientdb = new MongoClient(db);
   const dbName = "SquadJS";
   const dbCollection = "mainstats";
+  const responseSteam = await fetch(
+    `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApi}&steamids=${steamId}`
+  );
+  const dataSteam = await responseSteam.json();
+  const userInfo = dataSteam.response.players;
 
   try {
     await clientdb.connect();
@@ -18,19 +23,53 @@ async function getStatsOnDiscord(db, steamId, message) {
     //   return;
     // }
     if (!user) return;
-
-    let exampleEmbed = new EmbedBuilder()
+    // let exampleEmbed = new EmbedBuilder()
+    //   .setTitle(user.name.toString())
+    //   .setURL(`https://steamcommunity.com/profiles/${steamId}/`)
+    //   .addFields(
+    //     { name: "Kills", value: user.kills.toString(), inline: true },
+    //     { name: "Death", value: user.death.toString(), inline: true },
+    //     { name: "Revives", value: user.revives.toString(), inline: true },
+    //     { name: "TeamKills", value: user.teamkills.toString(), inline: true },
+    //     { name: "Bonuses", value: user.bonuses.toString(), inline: true },
+    //     { name: "K/D", value: user.kd.toString(), inline: true }
+    //   )
+    //   .setColor(0x0099ff);
+    let roles = Object.entries(user.roles);
+    let test = ["", 0];
+    roles.forEach((e) => {
+      console.log(e[1]);
+      if (test[1] < e[1]) {
+        test = e;
+      }
+    });
+    console.log(test[0].split("_").join("").toUpperCase());
+    const exampleEmbed = new EmbedBuilder()
+      .setColor(0x0099ff)
       .setTitle(user.name.toString())
       .setURL(`https://steamcommunity.com/profiles/${steamId}/`)
+      //.setDescription("Звание")
+      .setThumbnail(userInfo[0].avatarmedium)
       .addFields(
-        { name: "Kills", value: user.kills.toString(), inline: true },
-        { name: "Death", value: user.death.toString(), inline: true },
-        { name: "Revives", value: user.revives.toString(), inline: true },
-        { name: "TeamKills", value: user.teamkills.toString(), inline: true },
-        { name: "Bonuses", value: user.bonuses.toString(), inline: true },
-        { name: "K/D", value: user.kd.toString(), inline: true }
-      )
-      .setColor(0x0099ff);
+        // { name: "Очков опыта до повышения", value: "Очко" },
+        // { name: "\u200B", value: "\u200B" },
+        { name: "Убийств", value: user.kills.toString(), inline: true },
+        { name: "Смертей", value: user.death.toString(), inline: true },
+        { name: "Помощи", value: user.revives.toString(), inline: true },
+        { name: "Тимкилов", value: user.teamkills.toString(), inline: true },
+        { name: "Бонусов", value: user.bonuses.toString(), inline: true },
+        { name: "K/D", value: user.kd.toString(), inline: true },
+        {
+          name: "Время в игре",
+          value: user.squad.timeplayed.toString(),
+          inline: true,
+        },
+        {
+          name: "Лучший класс",
+          value: test[0].split("_").join("").toUpperCase(),
+          inline: true,
+        }
+      );
 
     message.reply({ embeds: [exampleEmbed] });
   } catch (e) {
