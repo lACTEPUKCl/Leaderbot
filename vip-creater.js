9da04f9bf51ab2a991924f141ff6cff72aff2c81
lACTEPUKCl <113736434+lACTEPUKCl@users.noreply.github.com>
2023-06-23 00:15:33 +0300

    Update vip-creater.js

--- vip-creater.js
+++ vip-creater.js
@@ -76,11 +76,11 @@ const vipCreater = async (steamID, nickname, summ, discordId) => {
 
           console.log("\x1b[33m", "\r\n Backup created AdminsBackup.cfg\r\n");
 
-           exec("../syncconfig.sh", (err, stdout, stderr) => {
-             if (err) {
-               console.error(err);
-               return;
-             }
+          exec("../syncconfig.sh", (err, stdout, stderr) => {
+           if (err) {
+             console.error(err);
+              return;
+           }
              console.log(stdout);
            });
         });
