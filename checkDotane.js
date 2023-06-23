75ac7e45540141f80cb0524a3f2364f06861429d
ACTEPUKC <makakakakala@gmail.com>
2023-06-23 22:12:48 +0300

    fix

--- checkDotane.js
+++ checkDotane.js
@@ -3,69 +3,86 @@ import fetch from "node-fetch";
 
 async function checkDonate(steamApi, tempSteamId, donateUrl, callback) {
   try {
-    let response = await fetch(donateUrl);
+    let retryCount = 0;
 
-    if (!response.ok) {
-      console.log(
-        "Не удалось получить список донатов. Повторная попытка через 10 секунд..."
-      );
-      await new Promise((resolve) => setTimeout(resolve, 10000));
-      response = await fetch(donateUrl);
-    }
+    while (retryCount < 3) {
+      let response = await fetch(donateUrl);
 
-    if (response.ok) {
-      const json = await response.json();
-      const steamIdRegex = /^https?:\/\/steamcommunity.com\/id\/(?<steamId>.*)/;
-      console.log("response", tempSteamId);
+      if (!response.ok) {
+        console.log(
+          "Не удалось получить список донатов. Повторная попытка через 10 секунд..."
+        );
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+        retryCount++;
+      } else {
+        const json = await response.json();
+        const steamIdRegex =
+          /^https?:\/\/steamcommunity.com\/id\/(?<steamId>.*)/;
+        console.log("response", tempSteamId);
 
-      for (const element of tempSteamId) {
-        const currentSteamId = element[2];
+        for (const element of tempSteamId) {
+          const currentSteamId = element[2];
 
-        for (const jsonEl of json.data) {
-          const comment = jsonEl.comment;
-          const steamID64 = comment.trim().match(/[0-9]{17}/);
-          const groupsId = comment.trim().match(steamIdRegex)?.groups;
-          const splitSteamId = groupsId?.steamId?.split("/")[0];
-          console.log("splitSteamId", splitSteamId);
-          console.log("groupsId", groupsId, typeof groupsId !== "undefined");
-          console.log(
-            "steamID64",
-            steamID64,
-            steamID64?.[0] === currentSteamId
-          );
+          for (const jsonEl of json.data) {
+            const comment = jsonEl.comment;
+            const steamID64 = comment.trim().match(/[0-9]{17}/);
+            const groupsId = comment.trim().match(steamIdRegex)?.groups;
+            const splitSteamId = groupsId?.steamId?.split("/")[0];
+            console.log("splitSteamId", splitSteamId);
+            console.log("groupsId", groupsId, typeof groupsId !== "undefined");
+            console.log(
+              "steamID64",
+              steamID64,
+              steamID64?.[0] === currentSteamId
+            );
 
-          if (typeof groupsId !== "undefined") {
-            try {
-              const resolveUrl = `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamApi}&vanityurl=${splitSteamId}`;
-              const responseSteam = await fetch(resolveUrl);
-              const dataSteam = await responseSteam.json();
-              console.log("dataSteam", dataSteam);
+            if (typeof groupsId !== "undefined") {
+              try {
+                const resolveUrl = `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamApi}&vanityurl=${splitSteamId}`;
+                const responseSteam = await fetch(resolveUrl);
+                const dataSteam = await responseSteam.json();
+                console.log("dataSteam", dataSteam);
 
-              if (
-                dataSteam.response.success === 1 &&
-                dataSteam.response.steamid === currentSteamId
-              ) {
-                fetchDonate(element, jsonEl);
-                console.log(`${currentSteamId} найден в списках донатов`);
-                break;
+                if (
+                  dataSteam.response.success === 1 &&
+                  dataSteam.response.steamid === currentSteamId
+                ) {
+                  fetchDonate(element, jsonEl);
+                  console.log(`${currentSteamId} найден в списках донатов`);
+                  break;
+                }
+              } catch (error) {
+                console.log("Не удалось получить steamID");
+                throw new Error(error);
               }
-            } catch (error) {
-              console.log("Не удалось получить steamID");
-              throw new Error(error);
+            }
+
+            if (steamID64?.[0] === currentSteamId) {
+              fetchDonate(element, jsonEl);
+              console.log(`${currentSteamId} найден в списках донатов`);
+              break;
             }
           }
 
-          if (steamID64?.[0] === currentSteamId) {
-            fetchDonate(element, jsonEl);
-            console.log(`${currentSteamId} найден в списках донатов`);
-            break;
-          }
+          console.log("Закончил проверку");
         }
 
-        console.log("Закончил проверку");
+        // Выходим из цикла, если найдены совпадения
+        if (retryCount === 0) {
+          break;
+        }
       }
-    } else {
-      console.log("Не удалось получить список донатов");
+
+      // Повторная попытка через 30 секунд
+      console.log(
+        "Совпадений не найдено. Повторная попытка через 30 секунд..."
+      );
+      await new Promise((resolve) => setTimeout(resolve, 30000));
+      retryCount++;
+    }
+
+    if (retryCount === 3) {
+      console.log("Совпадений не найдено");
     }
 
     callback();
