64606da700c58962b3e1e9422aceb7d3aab675fa
ACTEPUKC <makakakakala@gmail.com>
2023-08-05 16:56:07 +0300

    create charts tickrate

---
+++ chartInitialization.js
@@ -0,0 +1,37 @@
+import createChart from "./chartTickRate.js";
+
+async function chartInitialization(tickRateChannelId) {
+  const statsConfig = [
+    {
+      serverId: "1",
+      messageId: "1137382959930945576",
+      seconds: 1000,
+    },
+    {
+      serverId: "2",
+      messageId: "1137382998984118312",
+      seconds: 2000,
+    },
+    {
+      serverId: "3",
+      messageId: "1137383029514453144",
+      seconds: 4000,
+    },
+    {
+      serverId: "4",
+      messageId: "1137383059298193599",
+      seconds: 8000,
+    },
+  ];
+
+  const getChart = statsConfig.map((config) =>
+    createChart({
+      channel: tickRateChannelId,
+      serverId: config.serverId,
+      messageId: config.messageId,
+      seconds: config.seconds,
+    })
+  );
+}
+
+export default chartInitialization;
