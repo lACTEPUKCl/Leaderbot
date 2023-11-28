42e9443cce35d7a8cfe0deb321e4c69dafc989ab
ACTEPUKC <makakakakala@gmail.com>
2023-11-28 20:05:41 +0200

    fix leaderboard

--- utility/getTimePlayed.js
+++ utility/getTimePlayed.js
@@ -1,23 +1,11 @@
 import { config } from "dotenv";
-config();
 import axios from "axios";
 
-const apiURL_BM = "https://api.battlemetrics.com/players/match";
-const steamID_BM = "76561198979435382";
-const serverId = ["8592347", "19616682", "16963414", "19616726", "22478939"];
-const tokenBM = process.env.BATTLEMETRICS_API_KEY_RCON;
-const requestBody_BM = {
-  data: [
-    {
-      type: "identifier",
-      attributes: {
-        type: "steamID",
-        identifier: steamID_BM,
-      },
-    },
-  ],
-};
+config();
 
+const apiURL_BM = "https://api.battlemetrics.com/players/match";
+const serverId = ["8592347", "19616682", "16963414", "22478939"];
+const tokenBM = process.env.BATTLEMETRICS_API_KEY_RCON;
 const headers = {
   headers: {
     Authorization: `Bearer ${tokenBM}`,
@@ -25,23 +13,37 @@ const headers = {
   },
 };
 
-try {
-  const res_BM = await axios.post(apiURL_BM, requestBody_BM, headers);
-  const BMPlayerId = res_BM.data.data[0].id;
-  let playedTimeAllServersBM = 0;
+const getTimePlayed = async (steamID) => {
+  const requestBody_BM = {
+    data: [
+      {
+        type: "identifier",
+        attributes: {
+          type: "steamID",
+          identifier: steamID,
+        },
+      },
+    ],
+  };
 
-  for (let i = 0, res; i < serverId; i++) {
-    console.log("asd");
-    await axios
-      .get(
-        `https://api.battlemetrics.com/players/${BMPlayerId}/servers/${serverId[i]}`
-      )
-      .then(
-        (res) => (playedTimeAllServersBM += res.data.data.attributes.timePlayed)
-      )
-      .catch((err) => {});
+  try {
+    const res_BM = await axios.post(apiURL_BM, requestBody_BM, headers);
+    const BMPlayerId = res_BM.data.data[0].relationships.player.data.id;
+    let playedTimeAllServersBM = 0;
+
+    for (let i = 0; i < serverId.length; i++) {
+      try {
+        const res = await axios.get(
+          `https://api.battlemetrics.com/players/${BMPlayerId}/servers/${serverId[i]}`
+        );
+        playedTimeAllServersBM += res.data.data.attributes.timePlayed;
+      } catch (err) {}
+    }
+
+    return playedTimeAllServersBM;
+  } catch (error) {
+    console.error("Ошибка при получении данных из Battlemetrics");
   }
-  console.log(playedTimeAllServersBM);
-} catch (error) {
-  console.error("Ошибка при получении данных из Battlemetrics");
-}
+};
+
+export default getTimePlayed;
