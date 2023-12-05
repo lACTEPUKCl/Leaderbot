056190985344e7f858096dbf6613e469adb5f820
ACTEPUKC <makakakakala@gmail.com>
2023-12-05 20:53:14 +0200

    fix error

--- commands/stats.js
+++ commands/stats.js
@@ -28,8 +28,8 @@ const execute = async (interaction) => {
 
     const channelId = interaction.channelId;
 
-    if (channelId !== allowedChannelId) {
-      return await interaction.editReply({
+    if (channelId !== allowedChannelId || channelId !== allowedChannelId2) {
+      return await interaction.reply({
         content:
           "Команда доступна только VIP пользователям в канале 'Статистика'",
         ephemeral: true,
