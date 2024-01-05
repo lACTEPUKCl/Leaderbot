ba5871e88a9ac41eec7e43b054dae13f90bbd7e8
ACTEPUKC <makakakakala@gmail.com>
2024-01-05 21:37:40 +0200

    rebuild autodonation

--- utility/checkDonateNew.js
+++ utility/checkDonateNew.js
@@ -16,53 +16,59 @@ async function main(guildId, db) {
 
     for (const jsonEl of json.data) {
       const { id, what, comment, sum } = jsonEl;
-      const steamID64 = comment.match(/\b[0-9]{17}\b/)?.[0];
-      if (!steamID64) return;
-      if (!existingIds.includes(id.toString())) {
-        const clientdb = new MongoClient(db);
-        const dbName = "SquadJS";
-        const dbCollection = "mainstats";
+      const steamId64 = await getSteamId64(
+        steamApi,
+        comment,
+        async (steamId) => {
+          if (steamId) {
+            if (!existingIds.includes(id.toString())) {
+              const clientdb = new MongoClient(db);
+              const dbName = "SquadJS";
+              const dbCollection = "mainstats";
 
-        try {
-          await clientdb.connect();
-          const db = clientdb.db(dbName);
-          const collection = db.collection(dbCollection);
-          const user = await collection.findOne({ _id: steamID64 });
-          if (!user) return;
-          discordID = user.discordid;
-        } catch (e) {
-          console.error(e);
-        } finally {
-          await clientdb.close();
+              try {
+                await clientdb.connect();
+                const db = clientdb.db(dbName);
+                const collection = db.collection(dbCollection);
+                const user = await collection.findOne({ _id: steamId });
+                if (!user) return;
+                discordID = user.discordid;
+              } catch (e) {
+                console.error(e);
+              } finally {
+                await clientdb.close();
+              }
+
+              transaction.transactions.push({
+                id: `${id}`,
+                username: what,
+                steamID: steamId,
+              });
+              if (!discordID) return;
+
+              try {
+                const discordUser = await guildId.members.fetch(discordID);
+                const vipRole = guildId.roles.cache.find(
+                  (role) => role.name === "VIP"
+                );
+                creater.vipCreater(steamId, what, sum, discordID);
+                await discordUser.roles.add(vipRole);
+              } catch (error) {
+                console.log(error);
+              }
+
+              // message.channel.send({
+              //   content: `Игроку <@${message.author.id}> - выдан VIP статус, спасибо за поддержку!`,
+              // });
+
+              let newData = JSON.stringify(transaction);
+              fs.writeFile(`../transactionId.json`, newData, (err) => {
+                if (err) return;
+              });
+            }
+          }
         }
-
-        transaction.transactions.push({
-          id: `${id}`,
-          username: what,
-          steamID: steamID64,
-        });
-        if (!discordID) return;
-
-        try {
-          const discordUser = await guildId.members.fetch(discordID);
-          const vipRole = guildId.roles.cache.find(
-            (role) => role.name === "VIP"
-          );
-          creater.vipCreater(steamID64, what, sum, discordID);
-          await discordUser.roles.add(vipRole);
-        } catch (error) {
-          console.log(error);
-        }
-
-        // message.channel.send({
-        //   content: `Игроку <@${message.author.id}> - выдан VIP статус, спасибо за поддержку!`,
-        // });
-
-        let newData = JSON.stringify(transaction);
-        fs.writeFile(`../transactionId.json`, newData, (err) => {
-          if (err) return;
-        });
-      }
+      );
     }
   } catch (error) {
     console.error("Error:", error);
