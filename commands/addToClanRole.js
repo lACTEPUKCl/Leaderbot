19dcda4e8906d883f32ab6a74df6f1e151d22ded
ACTEPUKC <makakakakala@gmail.com>
2024-06-07 21:26:38 +0300

    add new command duel

--- commands/addToClanRole.js
+++ commands/addToClanRole.js
@@ -1,23 +1,96 @@
-import {
-  SlashCommandBuilder,
-  ButtonBuilder,
-  ActionRowBuilder,
-} from "discord.js";
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import fs from "fs";
+import { config } from "dotenv";
+config();
 
-const duel = new SlashCommandBuilder()
-  .setName("stvol")
-  .setDescription("Вызывает на дуэль другого члена сервера");
+const addToClanRoleCommand = new SlashCommandBuilder()
+  .setName("addtoclanrole")
+  .setDescription("Выдать пользователю клановую роль")
+  .setDefaultMemberPermissions(PermissionFlagsBits.CreatePrivateThreads)
+  .addUserOption((option) =>
+    option
+      .setName("name")
+      .setDescription("Напишите имя игрока в дискорде")
+      .setRequired(true)
+  );
+
 const execute = async (interaction) => {
-  const confirm = new ButtonBuilder()
-    .setCustomId(interaction.user.id)
-    .setLabel("Принять дуэль")
-    .setStyle(ButtonStyle.Danger);
+  try {
+    const user = interaction.user;
+    const userID = user.id;
+    const adminsCfgPath = process.env.ADMINS_URL;
+    const userName = interaction.options.getUser("name");
 
-  const row = new ActionRowBuilder().addComponents(confirm);
-  await interaction.reply({
-    content: `<@${interaction.user.id}> ждёт ответа на дуэль.`,
-    components: [row],
-  });
+    if (!userName) {
+      interaction.reply({
+        content: "Не удалось найти указанного пользователя.",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    fs.readFile(`${adminsCfgPath}Admins.cfg`, "utf8", async (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const lines = data.split("\n");
+
+      const clanRegex = /^\/\/CLAN \[(.+)] (\d+) do (.+)/;
+
+      lines.forEach((line) => {
+        const clanMatch = line.match(clanRegex);
+
+        if (clanMatch) {
+          const clanName = clanMatch[1];
+          const discordID = clanMatch[2];
+
+          if (discordID === userID) {
+            const guild = interaction.guild;
+            if (!guild) {
+              interaction.reply({
+                content: "Ошибка: команда не выполнима вне сервера.",
+                ephemeral: true,
+              });
+              return;
+            }
+
+            const member = guild.members.cache.get(userName.id);
+            if (!member) {
+              interaction.reply({
+                content: "Ошибка: указанный пользователь не найден на сервере.",
+                ephemeral: true,
+              });
+              return;
+            }
+
+            const role = guild.roles.cache.find(
+              (role) => role.name === `[${clanName}]`
+            );
+            if (role) {
+              member.roles.add(role);
+              interaction.reply({
+                content: `Роль клана "${clanName}" успешно выдана пользователю ${userName}`,
+                ephemeral: true,
+              });
+            } else {
+              interaction.reply({
+                content: `Не удалось найти роль для клана "${clanName}".`,
+                ephemeral: true,
+              });
+            }
+          }
+        }
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "Произошла ошибка.",
+      ephemeral: true,
+    });
+  }
 };
 
-export default { data: duel, execute };
+export default { data: addToClanRoleCommand, execute };
